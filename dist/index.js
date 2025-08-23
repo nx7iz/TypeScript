e607eb376397fcf08846cad8f0511d55ba4f6abe
Noman <nomansz500@gmail.com>
2025-08-23 12:19:58 +0500

    Coever fundamental topics

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,3 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let age = 20;
-if (age < 50)
-    age += 10;
-console.log(age);
 //# sourceMappingURL=index.js.map
---
+++ src/fundamentals.ts
@@ -0,0 +1,50 @@
+// Primitive
+// let sales = 123_456_789;
+// let course = "TypeScript";
+// let is_published = true;
+
+// any
+// let level;
+
+// function render(document) {
+//   console.log(document);
+// }
+
+// Arrays
+// let numbers: number[] = [];
+// numbers.forEach(n => n.)
+
+// Tuples -> Fixed length array (mainly key-value pairs)
+// let user: [number, string] = [1, 'Noman'];
+// user[0].toFixed(); // methods of number object
+// user[1].charAt; // methods, properties of string object
+
+// Enums -> list of related constants
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+
+// // PascalCase
+// const enum Size { Small = 1, Medium, Large };
+// let mySize : Size = Size.Small;
+// console.log(mySize);
+
+// Functions
+// function calculateTax(income: number, taxYear: number = 2022): number {
+//   if (taxYear < 2022) return income * 1.2;
+//   return income * 1.3;
+// }
+// calculateTax(10_000);
+
+// Objects
+let employee: {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void;
+} = {
+  id: 1,
+  name: "Noman",
+  retire: (date: Date) => {
+    console.log(date);
+  }
+};
